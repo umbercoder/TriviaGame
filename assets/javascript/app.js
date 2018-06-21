9e4dc3585467062f1caa969403578ad38ab63aa0
Brietta Smith <bri@Briettas-MacBook-Pro.local>
2018-06-21 08:58:09 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,52 @@
 
-// var Questions = [{
-//     question: "Which famous singer appeared in the movie Mad Max: Beyond Thunderdome?",
-//     choices: ["Whitney Huston", "Janet Jackson", "Tina Turner"],
-//     correctAnswer: 2
-//   },
-
-//   {
-//     question: "In which state was the first oil well drilled in the United States?",
-//     choices: ["Pennsylvania", "Texas", "Lousiana"],
-//     correctAnswer: 0
-//   },
-
-//   {
-//     question: "",
-//     choices: ["Bear", "Blue Whale", "Elephant"],
-//     correctAnswer: 1
-//   },
-
-//   {
-//     question: " What planet is closest to the sun?",
-//     choices: ["Mars", "Earth", "Mercury"],
-//     correctAnswer: 2
-//   },
-
-//   {
-//     question: "The United States state of Georgia is famous for what fruit?",
-//     choices: ["Pear", "Apples", "Peach"],
-//     correctAnswer: 2
- 
-//   }
-// ];
 
 var correct= 0;
 var wrong = 0;
 var noanswer = 0;
 
-
+$("#complete_bttn").on("click",function(){
+    timeUp()
+})
  
-function timeUp(){
+// $(document).ready(function(){
 
+
+
+// $("#thirdcontainer").hide();
+
+// var totalTime = 60;
+//    var timeStop = setInterval(timer, 1000);
+//    function timer() {
+
+//        if (totalTime == -1) {
+
+//            $("#time").append("<h2>Time's Up!</h2>");
+//            clearInterval(timeStop);
+//            results();
+//        }
+//        else {
+//            $("#time").html("<h2>Time Remaining: " + totalTime + " Seconds</h2>");
+//            totalTime--;
+//        }
+//    };
+
+// function timeStop(){
+
+// 		setInterval(countdown, 1000);
+
+// 	}
+
+
+function timeUp(){
+console.log("hello")
+var blank = undefined
     var q1 = $('input:radio[name="q1"]:checked').val();
     var q2 = $('input:radio[name="q2"]:checked').val();
     var q3 = $('input:radio[name="q3"]:checked').val();
     var q4 = $('input:radio[name="q4"]:checked').val();
     var q5 = $('input:radio[name="q5"]:checked').val();
 
-if(q1 == undefined){
+if(q1 == blank){
     noanswer++;
 }
 else if(q1 == "peach"){
@@ -106,14 +107,13 @@ else{
 
     $('#correctanswer').html(correct);
     $('#wronganswer').html(wrong);
-    $('#notanswered').html(noanswer);
+    $('#notanswered').html(noanswer)
 
-
-    // Show the completed Score Div
-    $("#lcontainer").show();
+    $("#thirdcontainer").show();
 
 }
 }
+// });
 
 
 
@@ -121,42 +121,3 @@ else{
 
 
 
-
-
-
-
-
-
-// Display();
-
-// var Display = function() {
-
-//     for (var j = 0; j < 5; j++) {
-//         $('<div class ="title">' + questions[j].question + '</div>');
-     
-//         for (var i = 0; i <= 3; i++) {
-//             $(questions[j]).append('<input type="radio"  value="' + questions[j].choices[i] + '"/><label >' + questions[j].choices[i] + '</label>');
-//         }
-//         $('.questions').prepend('<h1 />');
-    
-//     }
-// };
-
-// var total = $('#hit').on('click', function() {
-
-//     var correct = 0;
-//     var wrong = 0;
-//     var unAnswered = 0;
-
-//     for (var i = 0; i < 10; i++) {
-
-//         if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
-
-//             correct++;
-//         } else {
-//             wrong++;
-//         };
-//     };
-
-
-//  });  
